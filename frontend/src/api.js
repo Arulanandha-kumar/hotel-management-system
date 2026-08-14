116711573ae8663d04d02c7a981abcdfbb06ac38
arulananth <3454@oasys.com>
2026-08-14 14:34:29 +0530

    api server changes2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  //baseURL: "http://localhost:3000/api", // change if needed
+  //baseURL: "http://localhost:3000/api", // change if needed 
   baseURL: "https://hotel-management-system-0rbr.onrender.com/api"
 });
 
