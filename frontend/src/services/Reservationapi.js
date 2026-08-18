3acec8e4a450134ef0771aec9051f36604029149
arulananth <3454@oasys.com>
2026-08-18 10:25:41 +0530

    baseurl changes

--- frontend/src/services/Reservationapi.js
+++ frontend/src/services/Reservationapi.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.DEV
+  ? "http://localhost:3000/api"
+  : "/api";
+
 const api = axios.create({
   //baseURL: "http://localhost:3000/",
-  baseURL: "https://hotel-management-system.onrender.com",
+  // baseURL: "https://hotel-management-system.onrender.com",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
